bd6a958cd69b3b471ad4e4242b2b72cecde4576e
cv0cv0 <linux2.0@foxmail.com>
2019-01-09 11:19:11 +0800

    Initial commit

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice15FillPathView.kt
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice15FillPathView.kt
@@ -7,29 +7,23 @@ import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
-class Practice15FillPathView : View {
-    internal var paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    internal var pathPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    internal var path = Path()
-    internal var path1 = Path()
-    internal var path2 = Path()
-    internal var path3 = Path()
-
-    constructor(context: Context) : super(context) {}
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
-
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+class Practice15FillPathView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val pathPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val path = Path()
+    private val path1 = Path()
+    private val path2 = Path()
+    private val path3 = Path()
 
     init {
+        pathPaint.style = Paint.Style.STROKE
+
         path.moveTo(50f, 100f)
         path.rLineTo(50f, 100f)
         path.rLineTo(80f, -150f)
         path.rLineTo(100f, 100f)
         path.rLineTo(70f, -120f)
         path.rLineTo(150f, 80f)
-
-        pathPaint.style = Paint.Style.STROKE
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -40,6 +34,7 @@ class Practice15FillPathView : View {
         paint.style = Paint.Style.FILL_AND_STROKE
         paint.strokeWidth = 0f
         // 第一处：获取 Path
+        paint.getFillPath(path, path1)
         canvas.drawPath(path, paint)
 
         canvas.save()
@@ -51,6 +46,7 @@ class Practice15FillPathView : View {
         canvas.translate(0f, 200f)
         paint.style = Paint.Style.STROKE
         // 第二处：设置 Style 为 STROKE 后再获取 Path
+        paint.getFillPath(path, path2)
         canvas.drawPath(path, paint)
         canvas.restore()
 
@@ -63,6 +59,7 @@ class Practice15FillPathView : View {
         canvas.translate(0f, 400f)
         paint.strokeWidth = 40f
         // 第三处：Style 为 STROKE 并且线条宽度为 40 时的 Path
+        paint.getFillPath(path, path3)
         canvas.drawPath(path, paint)
         canvas.restore()
 
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice16TextPathView.kt
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice16TextPathView.kt
@@ -7,31 +7,23 @@ import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
-class Practice16TextPathView : View {
-    internal var paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    internal var pathPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    internal var textPath = Path()
-    internal var text = "Hello HenCoder"
-
-    constructor(context: Context) : super(context) {}
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
-
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+class Practice16TextPathView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val pathPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val textPath = Path()
+    private val text = "Hello HenCoder"
 
     init {
         paint.textSize = 120f
-
         // 使用 Paint.getTextPath() 来获取文字的 Path
+        paint.getTextPath(text, 0, text.length, 50f, 400f, textPath)
 
         pathPaint.style = Paint.Style.STROKE
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
         canvas.drawText(text, 50f, 200f, paint)
-
         canvas.drawPath(textPath, pathPaint)
     }
 }
